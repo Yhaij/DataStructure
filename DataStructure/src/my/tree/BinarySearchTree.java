5e6128b1d37cec586cf0c1edd4e491eff7759567
yhjiang_1 <214355245@qq.com>
2017-01-30 22:56:28 +0800

    BinarySearchTree

---
+++ DataStructure/src/my/tree/BinarySearchTree.java
@@ -0,0 +1,132 @@
+package my.tree;
+
+import java.util.DuplicateFormatFlagsException;
+import java.util.NoSuchElementException;
+
+public class BinarySearchTree<AnyType extends Comparable<? super AnyType>> {
+	private BinaryNode root;
+
+	private class BinaryNode {
+		private AnyType element;
+		private BinaryNode left;
+		private BinaryNode right;
+
+		public BinaryNode(AnyType element) {
+			// TODO Auto-generated constructor stub
+			this.element = element;
+			left = right = null;
+		}
+	}
+
+	public BinarySearchTree() {
+		// TODO Auto-generated constructor stub
+		root = null;
+	}
+
+	public boolean isEmpty() {
+		return root == null;
+	}
+
+	public void makeEmoty() {
+		root = null;
+	}
+
+	public void insert(AnyType x) {
+		insert(root, x);
+	}
+
+	public AnyType find(AnyType x) {
+		return elementAt(find(root, x));
+	}
+
+	public AnyType findMin() {
+		return elementAt(findMin(root));
+	}
+
+	public AnyType findMax() {
+		return elementAt(findMax(root));
+	}
+
+	public void remove(AnyType x) {
+		root = remove(root, x);
+	}
+
+	public void removeMin() {
+		root = removeMin(root);
+	}
+
+	private AnyType elementAt(BinaryNode t) {
+		return t == null ? null : t.element;
+	}
+
+	private void insert(BinaryNode t, AnyType x) {
+		if (t != null) {
+			if (t.element.compareTo(x) < 0) {
+				insert(t.left, x);
+			} else if (t.element.compareTo(x) > 0) {
+				insert(t.right, x);
+			} else
+				throw new DuplicateFormatFlagsException(x.toString()); // 插入相同的元素
+		} else {
+			t = new BinaryNode(x);
+		}
+	}
+
+	private BinaryNode find(BinaryNode t, AnyType x) {
+		while (t != null) {
+			if (t.element.compareTo(x) < 0)
+				t = t.left;
+			else if (t.element.compareTo(x) > 0) {
+				t = t.right;
+			} else
+				return t;
+		}
+		return null; // Not Found
+	}
+
+	private BinaryNode findMin(BinaryNode t) {
+		if (t != null)
+			while (t.left != null) {
+				t = t.left;
+			}
+		return t;
+	}
+
+	private BinaryNode findMax(BinaryNode t) {
+		if (t != null)
+			while (t.right != null) {
+				t = t.right;
+			}
+		return t;
+	}
+
+	private BinaryNode remove(BinaryNode t, AnyType x) {
+		if (t != null)
+			if (t.element.compareTo(x) < 0) {
+				t.left = remove(t.left, x);
+				return t;
+			} else if (t.element.compareTo(x) > 0) {
+				t.right = remove(t.right, x);
+				return t;
+			} else if(t.left != null && t.right != null){
+				t.element = elementAt(findMin(t.right));
+				t.right = removeMin(t.right);
+				return t;
+			}else{
+				return t.left == null?t.right:t.left;
+			}
+		else
+			throw new NoSuchElementException();
+	}
+
+	private BinaryNode removeMin(BinaryNode t) {
+		if (t != null) {
+			if (t.left != null) {
+				t.left = removeMin(t.left);
+				return t;
+			} else
+				return t.right;
+		} else
+			throw new NoSuchElementException();
+	}
+}
